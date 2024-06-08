f275cbf2660d7f4f70d7ff58e4cb3a137f0446d4
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-06-08 02:34:28 +0000

    chore: Add interactive web terminal with commands and social links

---
+++ js/commands.js
@@ -0,0 +1,87 @@
+var youtube = "https://www.youtube.com/@tech_apostle";
+var twitter = "https://www.twitter.com/kiss_johzzy/";
+var password = "techapostle";
+var linkedin = "https://www.linkedin.com/in/awejosepholaitan/";
+var instagram = "https://www.instagram.com/tech_apostle/";
+var github = "https://github.com/jossyboydgenius/";
+var email = 'mailto:awejosepholaitan@gmail.com';
+
+whois = [
+  "<br>",
+  "Hey, I'm Awe Joseph also known as TECH APOSTLE!👋",
+  "I'm a software engineer and cybersecurity specialist, known for",
+  "creating interactive and engaging web experiences like this one",
+  "I'm a passionate tech enthusiast with a diverse set of interests,",
+  "including machine learning and prompt engineering, among others.",
+  "Beyond my primary career path, I actively engage in these fascinating fields,",
+  " constantly seeking to expand my knowledge and skills.",
+  "Collaboration is a key part of my work ethic. ",
+  "I'm always open to joining forces with like-minded individuals on projects that have ",
+  "the potential to change lives. Whether it's developing an innovative application or working ",
+  "on a groundbreaking machine learning model, I'm all in for making a positive impact through technology.",
+  "<br>"
+];
+whoami = [
+  "<br>",
+  "The paradox of “Who am I?” is: We never know, but, we constantly find out.",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'youtube        <a href="' + youtube + '" target="_blank">youtube/@tech_apostle' + "</a>",
+  'twitter        <a href="' + twitter + '" target="_blank">twitter/kiss_johzzy' + '</a>',
+  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/awejosepholaitan' + "</a>",
+  'instagram      <a href="' + instagram + '" target="_blank">instagram/tech_apostle' + '</a>',
+  'github         <a href="' + github + '" target="_blank">github/jossyboydgenius' + "</a>",
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>   Only use if you\'re admin',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  '<a href="https://github.com/Jossyboydgenius?tab=repositories" target="_blank">View my projects on GitHub</a>',
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who is TechApostle?',
+  '<span class="command">whoami</span>         Who are you?',
+  '<span class="command">video</span>          View YouTube videos',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">secret</span>         Find the password',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">email</span>          Only email me if you have got a good reason',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  "<br>",
+];
+
+banner = [
+  '<span class="index">TechApostle Independent Entity. All rights reserved.</span>',
+  '<span class="index">Awe Joseph Olaitan Interactive Terminal, enjoy your exploration!</span>',
+  "<br>",
+ 
+"    █████████                                    ███████                                     █████     ",
+"   ███░░░░░███                                 ░░░░███                                     ░░███      ",
+"  ░███    ░███  █████ ███ █████  ██████           ░███   ██████   █████   ██████  ████████  ░███████  ",
+"  ░███████████ ░░███ ░███░░███  ███░░███          ░███  ███░░███ ███░░   ███░░███░░███░░███ ░███░░███ ",
+"  ░███░░░░░███  ░███ ░███ ░███ ░███████           ░███ ░███ ░███░░█████ ░███████  ░███ ░███ ░███ ░███ ",
+"  ░███    ░███  ░░███████████  ░███░░░      ███   ░███ ░███ ░███ ░░░░███░███░░░   ░███ ░███ ░███ ░███ ",
+"  █████   █████  ░░████░████   ░░██████    ░░████████  ░░██████  ██████ ░░██████  ░███████  ████ █████",
+"  ░░░░░   ░░░░░    ░░░░ ░░░░     ░░░░░░      ░░░░░░░░    ░░░░░░  ░░░░░░   ░░░░░░   ░███░░░  ░░░░ ░░░░░ ",
+"                                                                                   ░███                ",
+"                                                                                   █████               ",
+"                                                                                  ░░░░░         © 2024 ",
+  "<br>",
+  '<span class="color2">Welcome to my interactive web terminal.</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
